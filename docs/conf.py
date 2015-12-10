1e894510eb7eeda00b1caf013a0d253ba40a4de3
karim2021 <abdikarimhassan91@gmail.com>
2015-12-10 19:18:22 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # complexity documentation build configuration file, created by
-# sphinx-quickstart on Tue Jul  9 22:26:36 2013.
+# sphinx-quickstart on Tue Nov  24 22:26:36 2015.
 #
 # This file is execfile()d with the current directory set to its containing dir.
 #
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'open-helpdesk'
-copyright = u'2014, Simone Dalla'
+copyright = u'2015, Abdikarim Hassan'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -193,7 +193,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'open-helpdesk.tex', u'open-helpdesk Documentation',
-   u'Simone Dalla', 'manual'),
+   u'Abdikarim Hassan', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -223,7 +223,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'open-helpdesk', u'open-helpdesk Documentation',
-     [u'Simone Dalla'], 1)
+     [u'Abdikarim Hassan'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -237,7 +237,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'open-helpdesk', u'open-helpdesk Documentation',
-   u'Simone Dalla', 'open-helpdesk', 'One line description of project.',
+   u'Abdikarim Hassan', 'open-helpdesk', 'One line description of project.',
    'Miscellaneous'),
 ]
 
